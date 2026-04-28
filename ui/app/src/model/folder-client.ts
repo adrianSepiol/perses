39064862872c0eede04190d4b8de13ee58b91261
Adrian Sepiół <adisepko@gmail.com>
2026-04-28 15:19:00 +0200

    Use fetch wrapper.
    Signed-off-by: Adrian Sepiół <adisepko@gmail.com>

--- ui/app/src/model/folder-client.ts
+++ ui/app/src/model/folder-client.ts
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { fetchJson, FolderResource, StatusError } from '@perses-dev/core';
+import { fetch, fetchJson, FolderResource, StatusError } from '@perses-dev/core';
 import {
   useMutation,
   UseMutationResult,
